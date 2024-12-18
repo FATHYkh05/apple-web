80ca269b33eed568ffee08fb43bfc172925ad657
FATHY <fathy.kh05@gmail.com>
2024-12-19 01:09:44 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+let img = document.querySelector('.img');
+let container = document.querySelector('.container');
+
+function phones(x){
+    img.src = x;;
+}
+function colors(colors){
+    container.style.background = colors;
+
+}
